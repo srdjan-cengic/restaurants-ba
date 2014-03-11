d8c343613735ec4a26541f7f46929f362fabf9a2
srdjan <srdjan@olivebh.com>
2014-03-11 23:45:15 +0100

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+restourant_list = [
+	["Elemnt Illy", 
+	 "Svi gurmani, ali i ljubitelji kafe i caja istancanog cula okusa, 
+	 ponovo su dobili mjesto za izlazak po svojoj mjeri. Novootvoreni 
+	 restoran i caffe slasticarna Element Illy u Importanne centru od 
+	 jucer ima svoj potpuno novi izgled. 
+	 Ovo je mjesto dobre hrane i ukusnih slastica pripremljenih od 
+	 vrhunskih namirnica",
+	 "033 943-845",
+	 "https://www.facebook.com/pages/Element-Sarajevo/604630479567968",
+	 "https://www.youtube.com/watch?v=2daor2cv264"],
+	 ["Mala pivnica",
+	  "Pivnica Sarajevo je restoran u kojem se redovito sastaju razna 
+	  prijateljska drustva, a mi prepoznajemo njihove navike i potrebe. 
+	  Cuveni gradski boemi i umjetnici odabrali su Pivnicu Sarajevo 
+	  kao kultno mjesto koje im uljepsava dane.U ugodnom ambijentu 
+	  imaju priliku uzivati u nasim delicijama, vinima i rakijama, 
+	  i umjecu nasih  gostujucih muzicara. Ovo je mjesto za one koji 
+	  znaju da prepoznaju dobar ukus i stil.",
+	  "062 318-624",
+	  "https://www.facebook.com/pages/Pivnica-HS-Sarajevo/191359830900109?rf=191428997567001",
+	  "https://www.youtube.com/watch?v=2daor2cv264"
+	]
+]
+
+restourant_list.each do |name, desc, telephone, fb, video|
+	Restaurant.create(name: name, 
+					  description: desc, 
+					  telephone: telephone, 
+					  fb_page: fb, 
+					  video_url: video)
+end
