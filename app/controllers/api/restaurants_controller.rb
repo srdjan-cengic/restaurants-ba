8501f9ed79ba2877a7317c651efa2ded65859ea8
srdjan <srdjan@olivebh.com>
2014-03-13 15:30:44 +0100

    changes

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -11,43 +11,44 @@ class Api::RestaurantsController < ApplicationController
 	respond_to :json, :xml
 
 	def index
-		respond_to do |format|
-			format.html { 
-				head :method_not_allowed
-				return
-			}
+	  respond_to do |format|
+	    format.html { 
+		  head :method_not_allowed
+		  return
+		}
 
-			format.any(:json, :xml) {
-				restaurants = Restaurant.all
+	    format.any(:json, :xml) {
+	      restaurants = Restaurant.all
 
-				if restaurants.empty? 
-					head :not_found
-					return
-				end
+		  if restaurants.empty? 
+		    head :not_found
+		    return
+		  end
 
-				respond_with restaurants, status: :ok
-			}
-		end
+		  respond_with restaurants, status: :ok
+	    }
+	  end
 	end
 
 	def show
-		respond_to do |format|
-			format.html { 
-				head :method_not_allowed
-				return
-			}
+	  respond_to do |format|
+	    format.html { 
+		  head :method_not_allowed
+		  return
+		}
 
-			format.any(:json, :xml) {
-				begin
-					# something which might raise an exception
-					restaurant = Restaurant.find(params[:id])
-				rescue ActiveRecord::RecordNotFound
-					head :not_found
-					return
-				end
+		format.any(:json, :xml) {
+		  begin
+		    # something which might raise an exception
+			restaurant = Restaurant.find(params[:id])
+		  rescue ActiveRecord::RecordNotFound
+		    head :not_found
+			return
+		  end
 
-				respond_with restaurant, status: :ok
-			}
-		end
+		  respond_with restaurant, status: :ok
+		}
+	  end
 	end
+	
 end
