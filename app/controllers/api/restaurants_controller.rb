58d88a925d6a6b828e5e00bbc3b697969ad53654
srdjan <srdjan@olivebh.com>
2014-03-14 00:44:11 +0100

    post create new restaurant

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class Api::RestaurantsController < ApplicationController
+	before_filter :restaurant_params, only: :create
 	# The head method can be used to send responses with only headers to the browser. 
 	# It provides a more obvious alternative to calling render :nothing.
 
@@ -6,8 +7,9 @@ class Api::RestaurantsController < ApplicationController
 	# 405 = :method_not_allowed
 	# 404 = :not_found
 	# 200 = :ok
+	# 201 = :created
 
-	# default JSON format
+	# default is JSON format, set through routes
 	respond_to :json, :xml
 
 	def index
@@ -50,5 +52,40 @@ class Api::RestaurantsController < ApplicationController
 		}
 	  end
 	end
-	
+
+	# Be aware of: 
+	# ActionController::InvalidAuthenticityToken (ActionController::InvalidAuthenticityToken)
+	# 'Check to see who that client/IP is, it looks like they are using your site without 
+	# loading your views.'
+	def create
+		respond_to do |format|
+			format.html { 
+			  head :method_not_allowed
+			  return
+			}
+
+			format.any(:json, :xml) {
+				#The bang versions (e.g. save!) raise an exception if the record is invalid.
+				restourant = Restaurant.new(@permitted)
+				
+				begin
+					restourant.save!
+				rescue ActiveRecord::RecordInvalid
+					head :bad_request
+					return
+				end
+
+				head :created
+				return
+			}
+		end
+	end
+
+	private
+	  def restaurant_params
+	    @permitted = params.require(:restaurant).permit(:name, :description, :telephone, :fb_page, :video_url)
+	  end
 end
+
+#The @Consumes annotation is used to specify which MIME media types of representations a resource can accept, or consume, from the client. 
+#The @Produces annotation is used to specify the MIME media types or representations a resource can produce and send back to the client.
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ActiveRecord::Base
+	validates :name, :description, :telephone, :presence => true
+	validates :name, :telephone, :fb_page, :video_url, uniqueness: true
+	validates :name, length: { maximum: 60 }
+	validates :telephone, length: { maximum: 15 }
 end
