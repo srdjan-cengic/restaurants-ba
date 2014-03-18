327b061bda832488d9a596e916ff4acf43d8435f
DzenisaH <dzenisa.husic@gmail.com>
2014-03-19 00:10:46 +0100

    created DB

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140317233220) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "coupon_reservations", force: true do |t|
     t.integer  "user_id"
     t.integer  "coupon_id"
