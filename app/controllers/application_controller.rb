617e4f4c39c0fa8303726b113a8742492bf5e4e9
martinav <martina.vistica@gmail.com>
2014-03-30 19:48:23 +0200

    recaptcha pripreme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
  	'/superadmin/home'
   end
+  
   def after_sign_out_path_for(resource)
  	'/superadmin/login'
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
+
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :lockable
 
   has_many :favourites
+
+  def self.logins_before_captcha
+    5
+  end
 
 end
